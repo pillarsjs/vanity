56eb7ed5d0a90388ee74c70bf87bdb1dd1a1c3ae
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2017-11-30 14:05:32 +0000

    Added bootstrap & static setup for routes in Pillarsjs. #1

---
+++ server.js
@@ -0,0 +1,29 @@
+var project = require('pillars'),
+    GDB = require("goblindb");
+
+// Goblin Setup
+var goblinDB = GDB();
+
+// Starting the project
+project.services.get('http').configure({
+    port: process.env.PORT || 3000
+}).start();
+
+project.routes.add(new Route({
+    id: 'root',
+    path: '/',
+    directory: {
+        path: './public/index.html',
+        listing: true
+    }
+}));
+
+// Statics
+project.routes.add(new Route({
+    id: 'staticRoute',
+    path: '/*:path',
+    directory: {
+        path: './public',
+        listing: true
+    }
+}));
