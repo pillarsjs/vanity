194212742e3db6f91731cd8de94bba0670f7f3cf
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2017-12-01 19:21:49 +0000

    added cron jobs + api. #3

--- server.js
+++ server.js
@@ -7,21 +7,17 @@ var project = require('pillars'),
 // Goblin Setup
 var goblinDB = GDB();
 
-var data = goblinDB.get("packages") || [];
-
 // Starting the project
 project.services.get('http').configure({
     port: process.env.PORT || 3000
 }).start();
-
-
 
 // Root
 project.routes.add(new Route({
     id: 'root',
     path: '/'
 },function(gw){
-    gw.render('./public/index.html');
+    gw.file('./public/index.html');
 }));
 
 project.routes.add(new Route({
@@ -29,6 +25,7 @@ project.routes.add(new Route({
     path: '/api',
     cors: true
 }, function(gw) {
+    var data = goblinDB.get("packages") || [];
     gw.json(data, {
         deep: 10
     });
@@ -56,11 +53,11 @@ var pythonRocks = new Scheduled({
                 if (stdout) {
                     console.log('stdout: ' + stdout);
                 }
-            
+
                 if (stderr) {
                     console.log('stderr: ' + stderr);
                 }
-            
+
                 if (error) {
                     console.log('exec error: ' + error);
                 }
@@ -69,5 +66,33 @@ var pythonRocks = new Scheduled({
     }
 }).start();
 
-// Just for the first start.
-pythonRocks.launch();
+var goblinRocks = new Scheduled({
+    id: "goblinRocks",
+    pattern: "05 9 * * * *",
+    task: function(){
+        fs.readdir('./data/', function (err, files) {
+            if(err){
+                console.log("ERROR reading ./data/:", err);
+            } else {
+                var pkgData = []
+                files.forEach(function (file) {
+                    console.log(`- Adding ${file} data to goblinDB!`);
+                    pkgData.push(require("./data/"+file));
+                });
+                goblinDB.set(pkgData, "packages")
+            }
+        });
+    }
+})
+
+
+// Just for the first start
+if(!goblinDB.get("packages")){
+    console.log("GoblinDB is empty! Time to run all the cron jobs...")
+    pythonRocks.launch();
+    
+    // Setup a delay of 2 minutes for syncronization...
+    setTimeout(function(){
+        goblinRocks.launch()
+    }, 2000);
+}
